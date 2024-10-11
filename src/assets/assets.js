4e45023f3df615ac60d64b272003c0754a1df8d0
SpYrO272 <princeaj272@gmail.com>
2024-10-11 20:39:22 +0530

    final

---
+++ src/assets/assets.js
@@ -0,0 +1,50 @@
+import history_icon from './history_icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import question_icon from './question_icon.png'
+import setting_icon from './setting_icon.png'
+import bulb_icon from './bulb_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send_icon.png'
+import gemini_icon from './gemini_icon.png'
+import prof_icon from './prof_icon.png'
+import mesg_icon from'./mesg_icon.png'
+import mnu_icon from  './mnu_icon.png'
+import stting_icon from './stting_icon.png'
+import hlp_icon from './hlp_icon.png'
+import snd_icon from './snd_icon.png'
+import hist_icon from './hist_icon.png'
+import mc_icon from './mc_icon.png'
+import gal_icon from './gal_icon.png'
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    question_icon,
+    setting_icon,
+    bulb_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    youtube_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    gemini_icon,
+    prof_icon,
+    stting_icon,
+    mesg_icon,
+    mnu_icon,
+    hlp_icon,
+    snd_icon,
+    hist_icon,
+    mc_icon,
+    gal_icon
+}
